733a98b9e96da159220a33cbae6443c82b28fc8b
SumaiyaNishat <sumaiyanishat94@gmail.com>
2025-12-18 22:19:26 +0600

    setup router and added navbar

---
+++ src/routes/routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Components/Home/Home.jsx";
+import RootLayout from "../Layouts/RootLayout.jsx";
+import AllJobs from "../Components/AllJobs/AllJobs.jsx";
+import AddJob from "../Components/AddJob/AddJob.jsx";
+import MyTasks from "../Components/MyTasks/MyTasks.jsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: 'allJobs',
+        element: <AllJobs></AllJobs>
+      },
+      {
+        path: 'addJob',
+        element: <AddJob></AddJob>
+      },
+      {
+        path: 'myAcceptedTasks,',
+        element: <MyTasks></MyTasks>
+      },
+    ],
+  },
+]);
